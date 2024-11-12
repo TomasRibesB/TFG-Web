48884e44d846f8a16716d7705ca373ded60c0123
Tomas Ribes <tomasribesb@gmail.com>
2024-11-12 19:41:10 -0300

    login

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,6 +1,80 @@
+import { Button, TextField, Typography, Grid2, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <Grid2
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.paper", pb: 4 }}
+    >
+      <Grid2
+        className="card-shadow"
+        size={2.2}
+        sx={{
+          backgroundColor: "background.paper",
+          padding: 2,
+        }}
+      >
+        <Typography variant="h4" sx={{ mb: 1 }} align="center">
+          Iniciar sesión
+        </Typography>
+        <form>
+          <Grid2 container sx={{ p: 2 }}>
+            <Grid2 size={12}>
+              <TextField
+                label="Correo electrónico"
+                variant="outlined"
+                fullWidth
+                sx={{ mb: 2 }}
+                placeholder="correo@ejemplo.com"
+                InputProps={{
+                  style: {
+                    borderRadius: "50px",
+                  },
+                }}
+              />
+            </Grid2>
+            <Grid2 size={12}>
+              <TextField
+                label="Contraseña"
+                variant="outlined"
+                fullWidth
+                sx={{ mb: 4 }}
+                type="password"
+                InputProps={{
+                  style: {
+                    borderRadius: "50px",
+                  },
+                }}
+              />
+            </Grid2>
+            <Grid2 size={12}>
+              <Button
+                variant="contained"
+                fullWidth
+                sx={{
+                  borderRadius: "50px",
+                  textTransform: "none",
+                }}
+              >
+                Entrar
+              </Button>
+            </Grid2>
+            <Grid2 size={12} sx={{ mt: 2 }}>
+              <Typography variant="body2" align="center">
+                ¿No tienes cuenta?{" "}
+                <Link component={RouterLink} to="/auth/register">
+                  Regístrate
+                </Link>
+              </Typography>
+            </Grid2>
+          </Grid2>
+        </form>
+      </Grid2>
+    </Grid2>
+  );
+};
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,16 +1,26 @@
 import { createTheme } from "@mui/material";
-import { purple } from "@mui/material/colors";
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: purple[500],
+      main: "rgb(120, 69, 172)",
+      contrastText: "rgb(255, 255, 255)", // Equivalente a onPrimary
     },
     secondary: {
-      main: purple[300],
+      main: "rgb(102, 90, 111)",
+      contrastText: "rgb(255, 255, 255)", // Equivalente a onSecondary
     },
     error: {
-      main: "#f44336",
+      main: "rgb(186, 26, 26)",
+      contrastText: "rgb(255, 255, 255)", // Equivalente a onError
+    },
+    background: {
+      default: "rgb(255, 251, 255)",
+      paper: "#F8F3F9", // Equivalente a surface.main
+    },
+    text: {
+      primary: "rgb(29, 27, 30)", // Equivalente a onBackground
+      secondary: "rgb(74, 69, 78)", // Equivalente a onSurfaceVariant
     },
   },
 });
