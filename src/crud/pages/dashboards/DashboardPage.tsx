4b6e33c258d7314e184c6e3188bb77c15b192e54
Tomas Ribes <tomasribesb@gmail.com>
2024-11-13 17:00:22 -0300

    dashboard grid

--- src/crud/pages/dashboards/DashboardPage.tsx
+++ src/crud/pages/dashboards/DashboardPage.tsx
@@ -1,3 +1,57 @@
+import { Grid2 } from "@mui/material";
+
 export const DashboardPage = () => {
-  return <></>;
+  return (
+    <Grid2
+      container
+      spacing={7}
+      direction="column"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.paper", pb: 4 }}
+    >
+      <Grid2 container size={12} direction="row" sx={{ height: "20vh" }}>
+        <Grid2
+          className="card-shadow"
+          size={4}
+          sx={{
+            backgroundColor: "background.paper",
+            padding: 2,
+          }}
+        ></Grid2>
+        <Grid2
+          className="card-shadow"
+          size={4}
+          sx={{
+            backgroundColor: "background.paper",
+            padding: 2,
+          }}
+        ></Grid2>
+        <Grid2
+          className="card-shadow"
+          size={4}
+          sx={{
+            backgroundColor: "background.paper",
+            padding: 2,
+          }}
+        ></Grid2>
+      </Grid2>
+      <Grid2 container size={12} direction="row" sx={{ height: "60vh" }}>
+        <Grid2
+          className="card-shadow"
+          size={6}
+          sx={{
+            backgroundColor: "background.paper",
+            padding: 2,
+          }}
+        ></Grid2>
+        <Grid2
+          className="card-shadow"
+          size={6}
+          sx={{
+            backgroundColor: "background.paper",
+            padding: 2,
+          }}
+        ></Grid2>
+      </Grid2>
+    </Grid2>
+  );
 };
