81ee915c2e56a25cbcb780ba8fb3021b84489cda
Tomas Ribes <tomasribesb@gmail.com>
2025-03-06 14:31:25 -0300

    estructura

--- src/presentation/screens/crud/pages/TicketsPage.tsx
+++ src/presentation/screens/crud/pages/TicketsPage.tsx
@@ -3,27 +3,30 @@ import { Grid2 } from "@mui/material";
 export const TicketsPage = () => {
   return (
     <Grid2
-    container
-    spacing={2}
-    direction="row"
-    sx={{ backgroundColor: "primary.paper", pb: 4, height: "95.4%" }}
-  >
-    <Grid2
-      className="card-shadow"
-      size={4}
-      sx={{
-        backgroundColor: "background.paper",
-        padding: 2,
-      }}
-    ></Grid2>
-    <Grid2
-      className="card-shadow"
-      size={8}
-      sx={{
-        backgroundColor: "background.paper",
-        padding: 2,
-      }}
-    ></Grid2>
-  </Grid2>
-  )
-}
+      container
+      spacing={2}
+      sx={{ height: "95.4%", backgroundColor: "primary.paper", pb: 4 }}
+    >
+      <Grid2
+        size={{ xs: 12, md: 4, lg: 3 }}
+        className="card-shadow"
+        sx={{
+          backgroundColor: "background.paper",
+          p: 2,
+        }}
+      ></Grid2>
+      <Grid2
+        size={{
+          xs: 12,
+          md: 8,
+          lg: 9,
+        }}
+        className="card-shadow"
+        sx={{
+          backgroundColor: "background.paper",
+          p: 2,
+        }}
+      ></Grid2>
+    </Grid2>
+  );
+};
